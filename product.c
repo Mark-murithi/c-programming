1ce880c5a1026e3dc57ba0e183f60afe424c2f93
Mark-murithi <123064791+Mark-murithi@users.noreply.github.com>
2023-01-26 12:34:16 +0300

    Add files via upload

---
+++ product.c
@@ -0,0 +1,13 @@
+//arithmetic operators product
+#include<stdio.h>
+int main()
+{
+int x,y,prod;
+printf("\n enter the value of x :");
+scanf("%d",&x);
+printf("\n enter the value of y :");
+scanf("%d",&y);
+prod=x*y;
+printf("the product=%d",prod);
+return 0;
+}
